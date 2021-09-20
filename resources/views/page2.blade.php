606becf202d486f3a4d60be9b4ba88cd6858c286
Your Name <you@example.com>
2021-09-20 10:29:07 +0800

    first commit

---
+++ resources/views/page2.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Prelim Project</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    </head>
+    <body>
+
+        <div class="container d-flex aligns-items-center justify-content-center mt-5">
+            <p class="alert alert-danger h1">Page 2</p>
+        </div>
+        <div>
+            <nav  class="d-flex aligns-items-center justify-content-center" style="height:200px">
+                <ul class="d-flex list-unstyled ">
+                    <li><a class="nav-link mt-5" href="{{url('/')}}">Page 1</a></li>
+                    <li><a class="nav-link mt-5 h5 success" href="{{url('/page2')}}">Page 2</a></li>
+                    <li><a class="nav-link mt-5" href="{{url('/page3')}}">Page 3</a></li>
+                </ul>
+            </nav>
+        </div>
+
+
+
+        <div  class="d-flex aligns-items-center justify-content-center">
+            <footer class="badge bg-primary text-wrap">
+                <h5>Integrative Programming & Technology Prelim Project</h5>
+            </footer>
+        </div>
+        <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}">
+    </body>
+</html>
